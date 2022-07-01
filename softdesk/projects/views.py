1b28a97cc0c68a5fa81c57bc7898b3e6ed78cc29
Kévin Dérécusson <derec84@hotmail.fr>
2022-07-01 08:30:24 +0200

    Add django rest framework with a testing script

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Users
+from .serializers import UserSerializer
+
+
+@api_view(["GET"])
+def getData(request):
+    users = Users.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 
 def index(request):
