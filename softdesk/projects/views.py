6e27c296e1ba2cab1beb39d0f32198c285993c18
Kévin Dérécusson <derec84@hotmail.fr>
2022-07-19 17:45:05 +0200

    Rewriting views using generic class-based views

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,36 +1,13 @@
-from rest_framework import mixins
 from rest_framework import generics
 from .models import Users
 from .serializers import UserSerializer
 
 
-class UsersList(
-    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
-):
+class UsersList(generics.ListCreateAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class UserDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
