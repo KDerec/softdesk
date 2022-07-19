a837fa1a40aa78e3820ceb894801e333d22112e5
Kévin Dérécusson <derec84@hotmail.fr>
2022-07-19 16:53:06 +0200

    Make possible to create, edit or delete an user

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,24 +1,43 @@
-from django.http import HttpResponse
-from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Users
 from .serializers import UserSerializer
 
 
-@api_view(["GET"])
-def users_list(request):
-    users = Users.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+@api_view(["GET", "POST"])
+def users_list(request, format=None):
+    if request.method == "GET":
+        users = Users.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
-def user_detail(request, pk):
+@api_view(["GET", "PUT", "DELETE"])
+def user_detail(request, pk, format=None):
     try:
         user = Users.objects.get(pk=pk)
     except Users.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
