d0d01dad67899919b033da4c2ceca84750b157aa
Kévin Dérécusson <derec84@hotmail.fr>
2022-08-03 21:36:18 +0200

    Only display projects related to connected user

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -27,6 +27,13 @@ class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        id_projects_list = []
+        id_connected_user = self.request.user.id
+        for obj in Contributor.objects.filter(user_id=id_connected_user):
+            id_projects_list.append(obj.project_id)
+        return super().get_queryset().filter(id__in=id_projects_list)
+
     def perform_create(self, serializer):
         serializer.save(author_user=self.request.user)
         author_contributor = Contributor(
