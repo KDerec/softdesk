b867ab2a4c24a67d6e06ef2bf2c5a44e86ef060c
Kévin Dérécusson <derec84@hotmail.fr>
2022-08-05 21:04:03 +0200

    Use drf exceptions and convert project_id to int

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,8 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from django.http import Http404
-from django.core.exceptions import PermissionDenied
+from rest_framework.exceptions import NotFound, PermissionDenied
 from django.shortcuts import get_object_or_404
 from .models import Project, User, Contributor, Issue, Comment
 from .serializers import (
@@ -36,7 +35,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
         if self.detail == True:
             project_id = self.kwargs["pk"]
             check_project_exist_in_db(project_id)
-            if project_id in project_id_list:
+            if int(project_id) in project_id_list:
                 return super().get_queryset().filter(id__in=project_id_list)
             else:
                 raise PermissionDenied()
@@ -79,7 +78,7 @@ class ContributorViewSet(viewsets.ModelViewSet):
         try:
             filter_kwargs["pk"] = filtered_contributor.get().id
         except:
-            raise Http404
+            raise NotFound("Le numéro d'utilisateur renseigné n'existe pas.")
         obj = get_object_or_404(queryset, **filter_kwargs)
 
         # May raise a permission denied
@@ -113,19 +112,22 @@ class IssueViewSet(viewsets.ModelViewSet):
             raise PermissionDenied()
 
     def perform_create(self, serializer):
-        project = Project.objects.filter(id=self.kwargs["project_pk"]).get()
+        project = Project.objects.filter(id=int(self.kwargs["project_pk"])).get()
         serializer.save(project=project, author_user=self.request.user)
 
 
 def check_project_exist_in_db(project_id):
-    if project_id not in Project.objects.values_list("id", flat=True):
-        raise Http404
+    try:
+        if int(project_id) not in Project.objects.values_list("id", flat=True):
+            raise NotFound("Le numéro de projet indiqué n'existe pas.")
+    except ValueError:
+        raise NotFound("Le numéro de projet indiqué n'est pas un numéro.")
 
 
 def check_connected_user_is_project_contributor(self, project_id):
     connected_user_id = self.request.user.id
     if connected_user_id in Contributor.objects.filter(
-        project_id=project_id
+        project_id=int(project_id)
     ).values_list("user_id", flat=True):
         return True
     else:
